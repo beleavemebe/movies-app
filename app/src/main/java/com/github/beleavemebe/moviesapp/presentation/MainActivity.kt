1a0b4badf3f15b4a882c4246b8f6ac4fde065690
beleavemebe <cborzyy@gmail.com>
2022-04-20 19:55:26 +0300

    Move MainActivity.kt to `presentation` package

--- app/src/main/java/com/github/beleavemebe/moviesapp/presentation/MainActivity.kt
+++ app/src/main/java/com/github/beleavemebe/moviesapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.github.beleavemebe.moviesapp
+package com.github.beleavemebe.moviesapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.beleavemebe.moviesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
