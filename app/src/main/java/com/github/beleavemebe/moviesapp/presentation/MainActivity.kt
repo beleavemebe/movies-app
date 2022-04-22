0896a32e6136965c3715668c1dab2047c1e769d6
beleavemebe <cborzyy@gmail.com>
2022-04-22 13:35:16 +0300

    Setup navigation

--- app/src/main/java/com/github/beleavemebe/moviesapp/presentation/MainActivity.kt
+++ app/src/main/java/com/github/beleavemebe/moviesapp/presentation/MainActivity.kt
@@ -2,6 +2,8 @@ package com.github.beleavemebe.moviesapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.github.beleavemebe.moviesapp.R
 import com.github.beleavemebe.moviesapp.databinding.ActivityMainBinding
 
@@ -12,5 +14,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
+        binding.bottomNav.setupWithNavController(navHost.navController)
     }
 }
