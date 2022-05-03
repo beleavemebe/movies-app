3af1858e6a17a278b0f8c94e3c4dffc1ccfab395
beleavemebe <cborzyy@gmail.com>
2022-05-03 15:25:10 +0300

    Implement shimmer & Open link on click

--- app/src/main/java/com/github/beleavemebe/moviesapp/ui/movies/recycler/MovieReviewViewHolder.kt
+++ app/src/main/java/com/github/beleavemebe/moviesapp/ui/movies/recycler/MovieReviewViewHolder.kt
@@ -1,6 +1,9 @@
 package com.github.beleavemebe.moviesapp.ui.movies.recycler
 
-import androidx.paging.LoadState
+import android.content.Intent
+import android.net.Uri
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.github.beleavemebe.moviesapp.R
@@ -27,8 +30,31 @@ class MovieReviewViewHolder(
             .centerCrop()
             .placeholder(R.drawable.placeholder)
             .into(binding.ivImage)
+
+        binding.root.setOnClickListener {
+            openReview(movieReview.link.url)
+        }
+
+        binding.shimmerLayout.stopShimmer()
+        binding.shimmerLayout.isVisible = false
+    }
+
+    private fun openReview(reviewUrl: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(reviewUrl))
+        intent.addCategory(Intent.CATEGORY_BROWSABLE)
+        if (intent.resolveActivity(itemView.context.packageManager) != null) {
+            itemView.context.startActivity(intent)
+        } else {
+            Toast.makeText(
+                itemView.context,
+                R.string.unable_to_open_the_link,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
     private fun renderPlaceholder() {
+        binding.shimmerLayout.isVisible = true
+        binding.shimmerLayout.startShimmer()
     }
 }
