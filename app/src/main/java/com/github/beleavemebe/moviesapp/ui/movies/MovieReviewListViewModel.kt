afe13993d33b6851064b849ecb040c87cc3505ef
beleavemebe <cborzyy@gmail.com>
2022-05-03 14:00:24 +0300

    Wire up MovieReviewListViewModel with repository

--- app/src/main/java/com/github/beleavemebe/moviesapp/ui/movies/MovieReviewListViewModel.kt
+++ app/src/main/java/com/github/beleavemebe/moviesapp/ui/movies/MovieReviewListViewModel.kt
@@ -1,8 +1,20 @@
 package com.github.beleavemebe.moviesapp.ui.movies
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.github.beleavemebe.moviesapp.repository.MovieReviewRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
-class MovieReviewListViewModel @Inject constructor() : ViewModel()
+class MovieReviewListViewModel @Inject constructor(
+    movieReviewRepository: MovieReviewRepository,
+) : ViewModel() {
+    val content = movieReviewRepository.getMovieReviews()
+        .cachedIn(viewModelScope)
+        .stateIn(viewModelScope, SharingStarted.Lazily, PagingData.empty())
+}
