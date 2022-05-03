995fdb007f96ba4ff88646c35e1d24a6289ddfa8
beleavemebe <cborzyy@gmail.com>
2022-05-03 12:41:15 +0300

    Add `@Inject` to VM constructors

--- app/src/main/java/com/github/beleavemebe/moviesapp/ui/movies/MovieReviewListViewModel.kt
+++ app/src/main/java/com/github/beleavemebe/moviesapp/ui/movies/MovieReviewListViewModel.kt
@@ -2,6 +2,7 @@ package com.github.beleavemebe.moviesapp.ui.movies
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class MovieReviewListViewModel : ViewModel()
+class MovieReviewListViewModel @Inject constructor() : ViewModel()
--- app/src/main/java/com/github/beleavemebe/moviesapp/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/github/beleavemebe/moviesapp/ui/splash/SplashViewModel.kt
@@ -4,15 +4,15 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.Container
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
 import org.orbitmvi.orbit.syntax.simple.postSideEffect
 import org.orbitmvi.orbit.viewmodel.container
+import javax.inject.Inject
 
 @HiltViewModel
-class SplashViewModel : ViewModel(), ContainerHost<SplashState, SplashSideEffect> {
+class SplashViewModel @Inject constructor() : ViewModel(), ContainerHost<SplashState, SplashSideEffect> {
     override val container: Container<SplashState, SplashSideEffect> =
         container(SplashState())
 
